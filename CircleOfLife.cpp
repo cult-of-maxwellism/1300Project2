5be92aefe943a98d6ed6b5601e73bd434274629d
legosharkdan <legosharkdan@gmail.com>
2025-04-07 16:37:02 -0600

    modified:   .vscode/settings.json 	modified:   CircleOfLife.cpp 	new file:   a.out 	modified:   headers/helpers.h 	modified:   headers/libraries.h 	modified:   headers/players.h 	modified:   runnables/Board.cpp 	new file:   runnables/helper.cpp 	new file:   runnables/menu.cpp 	modified:   runnables/players.cpp

--- CircleOfLife.cpp
+++ CircleOfLife.cpp
@@ -2,9 +2,41 @@
 #include"headers/libraries.h"
 #include"headers/players.h"
 #include"headers/Board.h"
+#include <curses.h>
+#include<stdlib.h>
 
 //initial stuff
 int main () {
+    Board gameBoard(2);
+    gameBoard.initializeBoard();
+    gameBoard.displayBoard();
 
+    gameBoard.movePlayer(0);
+    gameBoard.getPlayerPosition(0);
+    gameBoard.displayBoard();
+    cout << endl << "+++++++++++++++++" << endl << endl << "new board:" << endl;
+
+    gameBoard.movePlayer(1);
+    gameBoard.getPlayerPosition(1);
+    gameBoard.displayBoard();
+    cout << endl << "+++++++++++++++++" << endl << endl << "new board:" << endl;
+
+    for (int i = 0; i < 10; i++) {
+        gameBoard.movePlayer(1);
+        gameBoard.getPlayerPosition(1);
+        
+        gameBoard.displayBoard();
+        cout << endl << "+++++++++++++++++" << endl << endl << "new board:" << endl;
+
+        if (i%2 == 0) {
+            for (int j = 0; j < 3; j++) {
+                gameBoard.movePlayer(0);
+            }
+        }
+    }
+    gameBoard.movePlayer(1);
+    gameBoard.getPlayerPosition(1);
+    gameBoard.displayBoard();
+    cout << endl << "+++++++++++++++++" << endl << endl << "new board:" << endl;
     return 0;
 }
